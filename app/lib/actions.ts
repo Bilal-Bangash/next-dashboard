89290eaffa288e3d2e8a3e9e91cc16047ab8e0a2
Muhammad Bilal Bangash <muhammadbilalbangash@hotmail.com>
2024-07-29 23:58:24 +0500

    learned how to improve accessibility in your forms with React Form Status and Server-side Validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,7 +57,11 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
   const rawFormData = Object.fromEntries(formData.entries())
   const validatedFields = UpdateInvoice.safeParse(rawFormData)
   if (!validatedFields.success) {
